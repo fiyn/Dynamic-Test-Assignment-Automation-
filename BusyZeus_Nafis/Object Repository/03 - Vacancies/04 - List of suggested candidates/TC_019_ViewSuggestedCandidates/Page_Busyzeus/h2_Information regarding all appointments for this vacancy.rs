d3dc64908ee5ea85dfa62c1385d042d5f92f7210
fiyn <alienafis@gmail.com>
2024-02-13 18:31:44 +0800

    Worked on 03 - Vacancies Page, 04 test case (TC_019)

---
+++ BusyZeus_Nafis/Object Repository/03 - Vacancies/04 - List of suggested candidates/TC_019_ViewSuggestedCandidates/Page_Busyzeus/h2_Information regarding all appointments for this vacancy.rs
@@ -0,0 +1,115 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Information regarding all appointments for this vacancy</name>
+   <tag></tag>
+   <elementGuidId>4c64a1e5-1fca-4e82-9ab6-c186cfba6db9</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='candidates']/app-vacancies-list-candidates/h2</value>
+      </entry>
+      <entry>
+         <key>CSS</key>
+         <value>h2.sub-headline.ng-star-inserted</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+      <webElementGuid>f9654c9e-8e5d-4456-851c-9af6ebe6cea7</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>sub-headline ng-star-inserted</value>
+      <webElementGuid>e992a897-8ab1-42b0-b100-31fc248e3f87</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value> Information regarding all appointments for this vacancy </value>
+      <webElementGuid>7d6439f7-2c21-4845-8da8-e6c8518cd2b2</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;candidates&quot;)/app-vacancies-list-candidates[@class=&quot;ng-star-inserted&quot;]/h2[@class=&quot;sub-headline ng-star-inserted&quot;]</value>
+      <webElementGuid>a494a165-2a30-4998-9617-42689051f78b</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='candidates']/app-vacancies-list-candidates/h2</value>
+      <webElementGuid>469027bd-a85a-471f-b41f-1908d6f1f58e</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='must know All'])[1]/following::h2[1]</value>
+      <webElementGuid>ab40aa49-4100-46c0-927f-6293a010aaab</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Skill Sets'])[1]/following::h2[1]</value>
+      <webElementGuid>a8024475-d8bb-4bfa-9702-03c96218c5ba</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='List of suggested candidates'])[2]/preceding::h2[1]</value>
+      <webElementGuid>88e7a82f-d83f-4463-b230-014e04171354</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>(.//*[normalize-space(text()) and normalize-space(.)='Match'])[1]/preceding::h2[1]</value>
+      <webElementGuid>ea01e0bb-c819-4899-816d-487c767c9430</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:neighbor</name>
+      <type>Main</type>
+      <value>//*/text()[normalize-space(.)='Information regarding all appointments for this vacancy']/parent::*</value>
+      <webElementGuid>3d6c67df-3b55-4b0d-b2d3-173661e10ff0</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//app-vacancies-list-candidates/h2</value>
+      <webElementGuid>bf41af91-839d-47e9-89be-91491b7eda8d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//h2[(text() = ' Information regarding all appointments for this vacancy ' or . = ' Information regarding all appointments for this vacancy ')]</value>
+      <webElementGuid>6627db59-c763-4c5d-83ca-b86322bdcb23</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
